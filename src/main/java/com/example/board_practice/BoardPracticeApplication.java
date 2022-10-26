84e1bb5911c5475000ed98e347bc60140563fa78
eunjung0203 <grace99985@gmail.com>
2022-10-27 02:48:57 +0900

    test2

--- src/main/java/com/example/board_practice/BoardPracticeApplication.java
+++ src/main/java/com/example/board_practice/BoardPracticeApplication.java
@@ -2,9 +2,7 @@ package com.example.board_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class BoardPracticeApplication {
 
