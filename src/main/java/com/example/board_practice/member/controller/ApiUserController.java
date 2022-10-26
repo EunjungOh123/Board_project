94ca471b222c52e30f5ce25a18cbf839446602f4
eunjung0203 <grace99985@gmail.com>
2022-10-27 02:54:15 +0900

    test33

---
+++ src/main/java/com/example/board_practice/member/controller/ApiUserController.java
@@ -0,0 +1,51 @@
+package com.example.board_practice.member.controller;
+
+import com.example.board_practice.common.model.ResponseResult;
+import com.example.board_practice.course.dto.TakeCourseDto;
+import com.example.board_practice.course.model.ServiceResult;
+import com.example.board_practice.course.model.TakeCourseInput;
+import com.example.board_practice.course.service.TakeCourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RequiredArgsConstructor
+@RestController
+public class ApiUserController {
+
+    private final TakeCourseService takeCourseService;
+
+    @PostMapping("/api/member/course/cancel.api")
+    public ResponseEntity<?> cancelCourse(Model model
+            , @RequestBody TakeCourseInput parameter
+            , Principal principal) {
+
+        String userId = principal.getName();
+
+        //내 강좌인지 확인
+        TakeCourseDto detail = takeCourseService.detail(parameter.getTakeCourseId());
+        if (detail == null) {
+            ResponseResult responseResult = new ResponseResult(false, "수강 신청 정보가 존재하지 않습니다.");
+            return ResponseEntity.ok().body(responseResult);
+        }
+
+        if (userId == null || !userId.equals(detail.getUserId())) {
+            ResponseResult responseResult = new ResponseResult(false, "본인의 수강 신청 정보만 취소할 수 있습니다.");
+            return ResponseEntity.ok().body(responseResult);
+        }
+
+        ServiceResult result = takeCourseService.cancel(parameter.getTakeCourseId());
+        if (!result.isResult()) {
+            ResponseResult responseResult = new ResponseResult(false, result.getMessage());
+            return ResponseEntity.ok().body(responseResult);
+        }
+
+        ResponseResult responseResult = new ResponseResult(true);
+        return ResponseEntity.ok().body(responseResult);
+    }
+}
